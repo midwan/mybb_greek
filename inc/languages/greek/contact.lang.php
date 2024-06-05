3e4f824c9f417f2c148298e84df07860cae4d24c
Dimitris Panokostas <midwan@gmail.com>
2024-06-05 18:53:04 +0200

    Translated contact

--- inc/languages/greek/contact.lang.php
+++ inc/languages/greek/contact.lang.php
@@ -1,26 +1,26 @@
 <?php
 /**
- * MyBB 1.8 English Language Pack
+ * MyBB 1.8 Greek Language Pack
  * Copyright 2014 MyBB Group, All Rights Reserved
  *
  */
 
-$l['contact'] = 'Contact Us';
-$l['contact_no_message'] = 'You have not provided a message to send.';
-$l['contact_no_subject'] = 'You are required to enter a subject.';
-$l['contact_no_email'] = 'You must enter a valid email address.';
-$l['contact_success_message'] = 'Your message has been successfully sent to the Administrator.';
-$l['contact_subject'] = 'Subject';
-$l['contact_subject_desc'] = 'Brief the topic of your message.';
-$l['contact_message'] = 'Message';
-$l['contact_message_desc'] = 'Describe your concern in this area in detail.';
+$l['contact'] = 'Επικοινωνήστε μαζί μας';
+$l['contact_no_message'] = 'Δεν έχετε εισάγει μήνυμα για αποστολή.';
+$l['contact_no_subject'] = 'Απαιτείται να συμπληρώσετε το θέμα.';
+$l['contact_no_email'] = 'Πρέπει να συμπληρώσετε μια έγκυρη διεύθυνση email.';
+$l['contact_success_message'] = 'Το μήνυμά σας έχει αποσταλεί στον διαχειριστή.';
+$l['contact_subject'] = 'Θέμα';
+$l['contact_subject_desc'] = 'Σύντομη περιγραφή του μηνύματός σας.';
+$l['contact_message'] = 'Μήνυμα';
+$l['contact_message_desc'] = 'Περιγράψτε το μήνυμά σας αναλυτικά, εδώ.';
 $l['contact_email'] = 'Email';
-$l['contact_email_desc'] = 'Enter your email so we can contact you back.';
-$l['contact_send'] = 'Send';
-$l['subject_too_long'] = 'The subject is too long. Please enter a subject shorter than {1} characters (currently {2}).';
-$l['message_too_short'] = 'The message is too short. Please enter a message longer than {1} characters (currently {2}).';
-$l['message_too_long'] = 'The message is too long. Please enter a message shorter than {1} characters (currently {2}).';
+$l['contact_email_desc'] = 'Είσάγετε τη διεύθυνση email σας, ώστε να μπορούμε να επικοινωνήσουμε μαζί σας.';
+$l['contact_send'] = 'Αποστολή';
+$l['subject_too_long'] = 'Το θέμα είναι πολύ μεγάλο. Παρακαλώ εισάγετε ένα θέμα μικρότερο από {1} χαρακτήρες (προς το παρόν {2}).';
+$l['message_too_short'] = 'Το μήνυμα είναι πολύ μικρό. Παρακαλώ εισάγετε ένα μήνυμα μετά τουλάχιστον {1} χαρακτήρες (προς το παρόν {2}).';
+$l['message_too_long'] = 'Το μήνυμα είναι πολύ μεγάλο. Παρακαλώ εισάγετε ένα μήνυμα μικρότερο από {1} χαρακτήρες (προς το παρόν {2}).';
 
-$l['error_stop_forum_spam_spammer'] = 'Sorry, {1} matches that of a known spammer and therefore your contact submission has been blocked.';
-$l['error_stop_forum_spam_fetching'] = 'Sorry, something went wrong verifying your message against a spammer database. Most likely the database couldn\'t be accessed. Please try again later.';
+$l['error_stop_forum_spam_spammer'] = 'Συγγνώμη, το email σας {1} ανιχνεύτηκε ως spammer στη βάση δεδομένων και η υποβολή της επικοινωνίας σας έχει αποκλειστεί.';
+$l['error_stop_forum_spam_fetching'] = 'Συγνώμη, κάτι πήγε στραβά κατά τον έλεγχο του email σας στη βάση δεδομένων ανεπιθύμητης αλληλογραφίας. Παρακαλώ, προσπαθήστε ξανά αργότερα.';
 
