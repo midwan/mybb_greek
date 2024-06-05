b8a86cdd982fdbe4a036daa43c2453cefac171f4
Dimitris Panokostas <midwan@gmail.com>
2024-06-05 21:45:33 +0200

    Translated editpost

--- inc/languages/greek/editpost.lang.php
+++ inc/languages/greek/editpost.lang.php
@@ -1,54 +1,54 @@
 <?php
 /**
- * MyBB 1.8 English Language Pack
+ * MyBB 1.8 Greek Language Pack
  * Copyright 2014 MyBB Group, All Rights Reserved
  *
  */
 
-$l['nav_editpost'] = "Edit Post";
-$l['edit_post'] = "Edit This Post";
-$l['delete_post'] = "Delete Post";
-$l['delete_q'] = "Delete?";
-$l['delete_1'] = "To delete this post, check the checkbox to the left and then click the button to the right.";
-$l['delete_2'] = "<b>Note:</b> If this post is the first post in a thread deleting it will result in deletion of the whole thread.";
-$l['subject'] = "Subject:";
-$l['your_message'] = "Your Message:";
-$l['post_options'] = "Post Options:";
-$l['editreason'] = "Edit Reason:";
-$l['options_sig'] = "<strong>Signature:</strong> include your signature. (registered users only)";
-$l['options_emailnotify'] = "<strong>Email Notification:</strong> receive an email whenever there is a new reply. (registered users only)";
-$l['options_disablesmilies'] = "<strong>Disable Smilies:</strong> disable smilies from showing in this post.";
-$l['preview_post'] = "Preview Post";
-$l['update_post'] = "Update Post";
-$l['poll'] = "Poll:";
-$l['poll_desc'] = "Optionally you may attach a poll to this thread.";
-$l['poll_check'] = "I want to post a poll";
-$l['num_options'] = "Number of options:";
-$l['max_options'] = "(Maximum: {1})";
-$l['delete_now'] = "Delete Now";
-$l['edit_time_limit'] = "Sorry but you cannot edit your post. The Administrator has set it so that posts can only be edited within {1} minutes of posting.";
-$l['no_prefix'] = "No Prefix";
+$l['nav_editpost'] = "Επεξεργασία Ανάρτησης";
+$l['edit_post'] = "Επεξεργασία Αυτής της Ανάρτησης";
+$l['delete_post'] = "Διαγραφή Ανάρτησης";
+$l['delete_q'] = "Διαγραφή;";
+$l['delete_1'] = "Για να διαγράψετε αυτήν την ανάρτηση, επιλέξτε το πλαίσιο ελέγχου στα αριστερά και κάντε κλικ στο κουμπί στα δεξιά.";
+$l['delete_2'] = "<b>Σημείωση:</b> Εάν αυτή η ανάρτηση είναι η πρώτη ανάρτηση σε ένα νήμα, η διαγραφή της θα οδηγήσει στη διαγραφή ολόκληρου του νήματος.";
+$l['subject'] = "Θέμα:";
+$l['your_message'] = "Το Μήνυμά σας:";
+$l['post_options'] = "Επιλογές Ανάρτησης:";
+$l['editreason'] = "Λόγος Επεξεργασίας:";
+$l['options_sig'] = "<strong>Υπογραφή:</strong> συμπερίληψη της υπογραφής σας. (μόνο για εγγεγραμμένους χρήστες)";
+$l['options_emailnotify'] = "<strong>Ειδοποίηση μέσω Email:</strong> λήψη email κάθε φορά που υπάρχει μια νέα απάντηση. (μόνο για εγγεγραμμένους χρήστες)";
+$l['options_disablesmilies'] = "<strong>Απενεργοποίηση Συμβολοσειρών:</strong> απενεργοποίηση των συμβολοσειρών από την εμφάνιση σε αυτήν την ανάρτηση.";
+$l['preview_post'] = "Προεπισκόπηση Ανάρτησης";
+$l['update_post'] = "Ενημέρωση Ανάρτησης";
+$l['poll'] = "Δημοσκόπηση:";
+$l['poll_desc'] = "Προαιρετικά, μπορείτε να προσθέσετε μια δημοσκόπηση σε αυτό το νήμα.";
+$l['poll_check'] = "Θέλω να δημοσιεύσω μια δημοσκόπηση";
+$l['num_options'] = "Αριθμός επιλογών:";
+$l['max_options'] = "(Μέγιστο: {1})";
+$l['delete_now'] = "Διαγραφή Τώρα";
+$l['edit_time_limit'] = "Λυπούμαστε, αλλά δεν μπορείτε να επεξεργαστείτε την ανάρτησή σας. Ο Διαχειριστής έχει ορίσει ώρα επεξεργασίας αναρτήσεων σε {1} λεπτά από την ανάρτηση.";
+$l['no_prefix'] = "Χωρίς Πρόθεμα";
 
-$l['redirect_nodelete'] = "The post was not deleted because you didn't check the \"Delete\" checkbox.";
-$l['redirect_norestore'] = "The post was not restored because you didn't check the \"Restore\" checkbox.";
-$l['redirect_postedited'] = "Thank you, this post has been edited.<br />";
-$l['redirect_postedited_redirect'] = "You will now be returned to the thread.";
-$l['redirect_postedited_poll'] = "Thank you, this post has been edited. <br />Because you opted to post a poll, you'll now be taken to the poll creation page.";
-$l['error_invalidpost'] = "Sorry, but you seem to have followed an invalid address. Please be sure the specified post exists and try again.";
-$l['redirect_threaddeleted'] = "Thank you, the thread has been deleted.<br />You will now be returned to the forum.";
-$l['redirect_postdeleted'] = "Thank you, the post has been deleted.<br />You will now be returned to the thread.";
-$l['redirect_threadrestored'] = "Thank you, the thread has been restored.<br />You will now be returned to the forum.";
-$l['redirect_postrestored'] = "Thank you, the post has been restored.<br />You will now be returned to the thread.";
-$l['redirect_threadclosed'] = "You cannot edit existing posts in this thread because it has been closed by a moderator.";
-$l['redirect_post_moderation'] = "The administrator has specified that all editing of posts require moderation. You will now be returned to the thread.";
-$l['redirect_thread_moderation'] = "The administrator has specified that all editing of threads require moderation. You will now be returned to the forum index.";
-$l['error_already_delete'] = "Sorry, but this post is already deleted.";
+$l['redirect_nodelete'] = "Η ανάρτηση δεν διαγράφηκε επειδή δεν επιλέξατε το πλαίσιο ελέγχου \"Διαγραφή\".";
+$l['redirect_norestore'] = "Η ανάρτηση δεν αποκαταστάθηκε επειδή δεν επιλέξατε το πλαίσιο ελέγχου \"Αποκατάσταση\".";
+$l['redirect_postedited'] = "Σας ευχαριστούμε, αυτή η ανάρτηση έχει επεξεργαστεί.<br />";
+$l['redirect_postedited_redirect'] = "Θα επιστρέψετε τώρα στο νήμα.";
+$l['redirect_postedited_poll'] = "Σας ευχαριστούμε, αυτή η ανάρτηση έχει επεξεργαστεί. <br />Επειδή επιλέξατε να δημοσιεύσετε μια δημοσκόπηση, θα μεταβείτε τώρα στη σελίδα δημιουργίας δημοσκόπησης.";
+$l['error_invalidpost'] = "Λυπούμαστε, αλλά φαίνεται ότι ακολουθήσατε μια μη έγκυρη διεύθυνση. Βεβαιωθείτε ότι η συγκεκριμένη ανάρτηση υπάρχει και δοκιμάστε ξανά.";
+$l['redirect_threaddeleted'] = "Σας ευχαριστούμε, το νήμα διαγράφηκε.<br />Θα επιστρέψετε τώρα στο φόρουμ.";
+$l['redirect_postdeleted'] = "Σας ευχαριστούμε, η ανάρτηση διαγράφηκε.<br />Θα επιστρέψετε τώρα στο νήμα.";
+$l['redirect_threadrestored'] = "Σας ευχαριστούμε, το νήμα αποκαταστάθηκε.<br />Θα επιστρέψετε τώρα στο φόρουμ.";
+$l['redirect_postrestored'] = "Σας ευχαριστούμε, η ανάρτηση αποκαταστάθηκε.<br />Θα επιστρέψετε τώρα στο νήμα.";
+$l['redirect_threadclosed'] = "Δεν μπορείτε να επεξεργαστείτε υπάρχουσες αναρτήσεις σε αυτό το νήμα επειδή έχει κλείσει από έναν διαχειριστή.";
+$l['redirect_post_moderation'] = "Ο διαχειριστής έχει καθορίσει ότι όλες οι επεξεργασίες αναρτήσεων απαιτούν ενδιάμεση επιθεώρηση. Θα επιστρέψετε τώρα στο νήμα.";
+$l['redirect_thread_moderation'] = "Ο διαχειριστής έχει καθορίσει ότι όλες οι επεξεργασίες νημάτων απαιτούν ενδιάμεση επιθεώρηση. Θα επιστρέψετε τώρα στην αρχική σελίδα του φόρουμ.";
+$l['error_already_delete'] = "Λυπούμαστε, αλλά αυτή η ανάρτηση έχει ήδη διαγραφεί.";
 
-$l['thread_deleted'] = "Deleted Thread Permanently";
-$l['post_deleted'] = "Deleted Post Permanently";
-$l['thread_soft_deleted'] = "Soft Deleted Thread";
-$l['post_soft_deleted'] = "Soft Deleted Post";
-$l['thread_restored'] = "Restored Thread";
-$l['post_restored'] = "Restored Post";
+$l['thread_deleted'] = "Οριστικά Διαγραμμένο Νήμα";
+$l['post_deleted'] = "Οριστικά Διαγραμμένη Ανάρτηση";
+$l['thread_soft_deleted'] = "Απαλοιφθέν Νήμα";
+$l['post_soft_deleted'] = "Απαλοιφθείσα Ανάρτηση";
+$l['thread_restored'] = "Ανακτηθέν Νήμα";
+$l['post_restored'] = "Ανακτηθείσα Ανάρτηση";
 
-$l['error_already_deleted'] = 'The selected post has already been deleted.';
+$l['error_already_deleted'] = 'Η επιλεγμένη ανάρτηση έχει ήδη διαγραφεί.';
