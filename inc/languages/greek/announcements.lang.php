f40b2040a3ae1997004de054b66953e921045cd4
Dimitris Panokostas <midwan@gmail.com>
2024-06-05 22:42:36 +0200

    Harmonize previous translations with the same terms

--- inc/languages/greek/announcements.lang.php
+++ inc/languages/greek/announcements.lang.php
@@ -5,12 +5,12 @@
  *
  */
 
-$l['nav_announcements'] = "Ανακοίνωση Συζήτησης";
+$l['nav_announcements'] = "Ανακοινώσεις Φόρουμ";
 $l['announcements'] = "Ανακοίνωση";
-$l['forum_announcement'] = "Ανακοίνωση Συζήτησης: {1}";
-$l['error_invalidannouncement'] = "Η ανακοίνωση που προσδιορίσατε είναι άκυρη.";
+$l['forum_announcement'] = "Ανακοίνωση Φόρουμ: {1}";
+$l['error_invalidannouncement'] = "Η καθορισμένη ανακοίνωση δεν είναι έγκυρη.";
 
-$l['announcement_edit'] = "Επεξεργασία της ανακοίνωσης";
-$l['announcement_qdelete'] = "Διαγραφή της ανακοίνωσης";
-$l['announcement_quickdelete_confirm'] = "Είστε σίγουροι ότι θέλετε να διαγράψετε την ανακοίνωση;";
+$l['announcement_edit'] = "Επεξεργασία αυτής της ανακοίνωσης";
+$l['announcement_qdelete'] = "Διαγραφή αυτής της ανακοίνωσης";
+$l['announcement_quickdelete_confirm'] = "Είστε βέβαιοι ότι θέλετε να διαγράψετε αυτήν την ανακοίνωση;";
 
