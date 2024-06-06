bed1f59fce886c479a2a569b246dbaf55f1154b3
Dimitris Panokostas <midwan@gmail.com>
2024-06-06 18:59:57 +0200

    translated tools_warninglog

--- inc/languages/greek/admin/tools_warninglog.lang.php
+++ inc/languages/greek/admin/tools_warninglog.lang.php
@@ -1,53 +1,53 @@
 <?php
 /**
- * MyBB 1.8 English Language Pack
+ * MyBB 1.8 Greek Language Pack
  * Copyright 2014 MyBB Group, All Rights Reserved
  *
  */
 
 
-$l['warning_logs'] = "Warning Logs";
-$l['warning_logs_desc'] = "This section allows you to view a history of warnings issued to users.";
-$l['warned_user'] = "Warned User";
-$l['warning'] = "Warning";
-$l['date_issued'] = "Date Issued";
-$l['expires'] = "Expires";
-$l['expiry_date'] = "Expiry Date";
-$l['issued_date'] = "Issued Date";
-$l['issued_by'] = "Issued By";
-$l['details'] = "Details";
-$l['filter_warning_logs'] = "Filter Warning Logs";
-$l['filter_warned_user'] = "Warned user:";
-$l['filter_issued_by'] = "Warning issued by:";
-$l['filter_reason'] = "Reason contains:";
-$l['sort_by'] = "Sort By:";
-$l['results_per_page'] = "Results Per Page:";
-$l['view'] = "View";
-$l['no_warning_logs'] = "There are no warning logs to view.";
-$l['revoked'] = "Revoked ";
-$l['post'] = "Post";
+$l['warning_logs'] = "Αρχεία Προειδοποιήσεων";
+$l['warning_logs_desc'] = "Αυτή η ενότητα σας επιτρέπει να προβάλλετε το ιστορικό των προειδοποιήσεων που έχουν εκδοθεί σε χρήστες.";
+$l['warned_user'] = "Χρήστης που προειδοποιήθηκε";
+$l['warning'] = "Προειδοποίηση";
+$l['date_issued'] = "Ημερομηνία Έκδοσης";
+$l['expires'] = "Λήγει";
+$l['expiry_date'] = "Ημερομηνία Λήξης";
+$l['issued_date'] = "Ημερομηνία Έκδοσης";
+$l['issued_by'] = "Έκδοση από";
+$l['details'] = "Λεπτομέρειες";
+$l['filter_warning_logs'] = "Φιλτράρισμα Αρχείων Προειδοποιήσεων";
+$l['filter_warned_user'] = "Χρήστης που προειδοποιήθηκε:";
+$l['filter_issued_by'] = "Προειδοποίηση εκδόθηκε από:";
+$l['filter_reason'] = "Ο λόγος περιέχει:";
+$l['sort_by'] = "Ταξινόμηση κατά:";
+$l['results_per_page'] = "Αποτελέσματα ανά σελίδα:";
+$l['view'] = "Προβολή";
+$l['no_warning_logs'] = "Δεν υπάρχουν αρχεία προειδοποιήσεων για προβολή.";
+$l['revoked'] = "Ανακλήθηκε ";
+$l['post'] = "Δημοσίευση";
 
-$l['asc'] = "Ascending";
-$l['desc'] = "Descending";
+$l['asc'] = "Αύξουσα";
+$l['desc'] = "Φθίνουσα";
 
-$l['in'] = "in";
-$l['order'] = "order";
+$l['in'] = "σε";
+$l['order'] = "σειρά";
 
-$l['warning_details'] = "Warning Details";
-$l['warning_note'] = "Administrative Notes";
-$l['already_expired'] = "Expired";
-$l['warning_revoked'] = "Revoked";
-$l['warning_active'] = "Active";
-$l['error_invalid_warning'] = "An invalid warning was specified.";
+$l['warning_details'] = "Λεπτομέρειες Προειδοποίησης";
+$l['warning_note'] = "Διαχειριστικές Σημειώσεις";
+$l['already_expired'] = "Έχει λήξει";
+$l['warning_revoked'] = "Ανακλήθηκε";
+$l['warning_active'] = "Ενεργή";
+$l['error_invalid_warning'] = "Δόθηκε μια μη έγκυρη προειδοποίηση.";
 
-$l['revoke_warning'] = "Revoke this Warning";
-$l['revoke_warning_desc'] = "To revoke this warning please enter a reason below. This will not remove any bans or suspensions imposed by this warning.";
-$l['reason'] = "Reason:";
-$l['warning_is_revoked'] = "This warning has been revoked";
-$l['revoked_by'] = "Revoked by:";
-$l['date_revoked'] = "Date Revoked:";
-$l['error_already_revoked'] = "This warning has already been revoked.";
-$l['error_no_revoke_reason'] = "You did not enter a reason as to why you want to revoke this warning.";
-$l['redirect_warning_revoked'] = "This warning has been revoked and the users warning points decreased successfully.";
+$l['revoke_warning'] = "Ανάκληση αυτής της Προειδοποίησης";
+$l['revoke_warning_desc'] = "Για να ανακληθεί αυτή η προειδοποίηση, εισαγάγετε έναν λόγο παρακάτω. Αυτό δεν θα αφαιρέσει καμία απαγόρευση ή αναστολή που επιβλήθηκε από αυτήν την προειδοποίηση.";
+$l['reason'] = "Λόγος:";
+$l['warning_is_revoked'] = "Αυτή η προειδοποίηση έχει ανακληθεί";
+$l['revoked_by'] = "Ανακλήθηκε από:";
+$l['date_revoked'] = "Ημερομηνία Ανάκλησης:";
+$l['error_already_revoked'] = "Αυτή η προειδοποίηση έχει ήδη ανακληθεί.";
+$l['error_no_revoke_reason'] = "Δεν καταχωρήσατε λόγο για τον οποίο θέλετε να ανακληθεί αυτή η προειδοποίηση.";
+$l['redirect_warning_revoked'] = "Αυτή η προειδοποίηση έχει ανακληθεί και οι πόντοι προειδοποίησης του χρήστη μειώθηκαν με επιτυχία.";
 
-$l['warning_points'] = "({1} points)";
+$l['warning_points'] = "({1} πόντοι)";
