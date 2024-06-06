b2278bb08e4e7e7b88d08fd14a2153a5d7cd747c
Dimitris Panokostas <midwan@gmail.com>
2024-06-06 17:26:42 +0200

    translated config_plugins

--- inc/languages/greek/admin/config_plugins.lang.php
+++ inc/languages/greek/admin/config_plugins.lang.php
@@ -1,52 +1,52 @@
 <?php
 /**
- * MyBB 1.8 English Language Pack
+ * MyBB 1.8 Greek Language Pack
  * Copyright 2014 MyBB Group, All Rights Reserved
  *
  */
 
-$l['plugins'] = "Plugins";
-$l['plugins_desc'] = "This section allows you to activate, deactivate, and manage the plugins that you have uploaded to your forum's <strong>inc/plugins</strong> directory. To hide a plugin from view, but not lose any stored information from it, click the Deactivate link.";
-$l['plugin_updates'] = "Plugin Updates";
-$l['plugin_updates_desc'] = "This section allows you to check for updates on all your plugins.";
-$l['browse_plugins'] = "Browse Plugins";
-$l['browse_plugins_desc'] = "Here you may browse the official MyBB modifications site for plugins compatible with your series of MyBB.";
-$l['browse_all_plugins'] = "Browse All Plugins";
+$l['plugins'] = "Πρόσθετα";
+$l['plugins_desc'] = "Αυτή η ενότητα σας επιτρέπει να ενεργοποιήσετε, απενεργοποιήσετε και διαχειριστείτε τα πρόσθετα που έχετε μεταφορτώσει στον κατάλογο <strong>inc/plugins</strong> του φόρουμ σας. Για να αποκρύψετε ένα πρόσθετο από την προβολή, αλλά χωρίς να χάσετε καμία αποθηκευμένη πληροφορία από αυτό, κάντε κλικ στον σύνδεσμο Απενεργοποίηση.";
+$l['plugin_updates'] = "Ενημερώσεις Προσθέτων";
+$l['plugin_updates_desc'] = "Αυτή η ενότητα σας επιτρέπει να ελέγξετε για ενημερώσεις σε όλα τα πρόσθετά σας.";
+$l['browse_plugins'] = "Περιήγηση στα Πρόσθετα";
+$l['browse_plugins_desc'] = "Εδώ μπορείτε να περιηγηθείτε στον επίσημο ιστότοπο τροποποιήσεων του MyBB για πρόσθετα που είναι συμβατά με τη σειρά του MyBB που χρησιμοποιείτε.";
+$l['browse_all_plugins'] = "Περιήγηση σε Όλα τα Πρόσθετα";
 
-$l['plugin'] = "Plugin";
-$l['active_plugin'] = "Active Plugins";
-$l['inactive_plugin'] = "Inactive Plugins";
-$l['your_version'] = "Your Version";
-$l['latest_version'] = "Latest Version";
-$l['download'] = "Download";
-$l['deactivate'] = "Deactivate";
-$l['activate'] = "Activate";
-$l['install_and_activate'] = "Install &amp; Activate";
-$l['uninstall'] = "Uninstall";
-$l['created_by'] = "Created by";
-$l['no_plugins'] = "There are no plugins on your forum at this time.";
-$l['no_active_plugins'] = "There are no active plugins on your forum.";
-$l['no_inactive_plugins'] = "There are no inactive plugins available.";
+$l['plugin'] = "Πρόσθετο";
+$l['active_plugin'] = "Ενεργά Πρόσθετα";
+$l['inactive_plugin'] = "Ανενεργά Πρόσθετα";
+$l['your_version'] = "Η Έκδοσή σας";
+$l['latest_version'] = "Τελευταία Έκδοση";
+$l['download'] = "Λήψη";
+$l['deactivate'] = "Απενεργοποίηση";
+$l['activate'] = "Ενεργοποίηση";
+$l['install_and_activate'] = "Εγκατάσταση &amp; Ενεργοποίηση";
+$l['uninstall'] = "Απεγκατάσταση";
+$l['created_by'] = "Δημιουργήθηκε από";
+$l['no_plugins'] = "Δεν υπάρχουν πρόσθετα στο φόρουμ σας αυτή τη στιγμή.";
+$l['no_active_plugins'] = "Δεν υπάρχουν ενεργά πρόσθετα στο φόρουμ σας.";
+$l['no_inactive_plugins'] = "Δεν υπάρχουν ανενεργά πρόσθετα διαθέσιμα.";
 
-$l['plugin_incompatible'] = "This plugin is incompatible with MyBB {1}";
+$l['plugin_incompatible'] = "Αυτό το πρόσθετο δεν είναι συμβατό με το MyBB {1}";
 
-$l['recommended_plugins_for_mybb'] = "Recommended Plugins for MyBB {1}";
-$l['browse_results_for_mybb'] = "Browse Results for MyBB {1}";
-$l['search_for_plugins'] = "Search for Plugins";
-$l['search'] = "Search";
+$l['recommended_plugins_for_mybb'] = "Προτεινόμενα Πρόσθετα για το MyBB {1}";
+$l['browse_results_for_mybb'] = "Αναζήτηση Αποτελεσμάτων για το MyBB {1}";
+$l['search_for_plugins'] = "Αναζήτηση για Πρόσθετα";
+$l['search'] = "Αναζήτηση";
 
-$l['error_vcheck_no_supported_plugins'] = "None of the plugins installed support version checking.";
-$l['error_vcheck_communications_problem'] = "There was a problem communicating with the MyBB modifications version server. Please try again in a few minutes.";
-$l['error_vcheck_vulnerable'] = "[Vulnerable plugin]:";
-$l['error_vcheck_vulnerable_notes'] = "This submission has currently been marked as vulnerable by the MyBB Staff. We recommend complete removal of this modification. Please see the notes below: ";
-$l['error_no_input'] = "Error code 1: No input specified.";
-$l['error_no_pids'] = "Error code 2: No plugin ids specified.";
-$l['error_communication_problem'] = "There was a problem communicating with the MyBB modifications server. Please try again in a few minutes.";
-$l['error_invalid_plugin'] = "The selected plugin does not exist.";
-$l['error_no_results_found'] = "No results were found for the specified keywords.";
+$l['error_vcheck_no_supported_plugins'] = "Κανένα από τα εγκατεστημένα πρόσθετα δεν υποστηρίζει έλεγχο έκδοσης.";
+$l['error_vcheck_communications_problem'] = "Παρουσιάστηκε πρόβλημα κατά την επικοινωνία με τον διακομιστή έκδοσης τροποποιήσεων του MyBB. Παρακαλούμε δοκιμάστε ξανά σε λίγα λεπτά.";
+$l['error_vcheck_vulnerable'] = "[Ευπάθεια πρόσθετου]:";
+$l['error_vcheck_vulnerable_notes'] = "Αυτή η υποβολή έχει χαρακτηριστεί ως ευπαθής από το προσωπικό του MyBB. Συνιστούμε την πλήρη απεγκατάσταση αυτής της τροποποίησης. Παρακαλούμε δείτε τις παρακάτω σημειώσεις: ";
+$l['error_no_input'] = "Κωδικός σφάλματος 1: Δεν έχει καθοριστεί είσοδος.";
+$l['error_no_pids'] = "Κωδικός σφάλματος 2: Δεν έχουν καθοριστεί αναγνωριστικά πρόσθετων.";
+$l['error_communication_problem'] = "Παρουσιάστηκε πρόβλημα κατά την επικοινωνία με τον διακομιστή τροποποιήσεων του MyBB. Παρακαλούμε δοκιμάστε ξανά σε λίγα λεπτά.";
+$l['error_invalid_plugin'] = "Το επιλεγμένο πρόσθετο δεν υπάρχει.";
+$l['error_no_results_found'] = "Δεν βρέθηκαν αποτελέσματα για τις καθορισμένες λέξεις-κλειδιά.";
 
-$l['success_plugins_up_to_date'] = "Congratulations, all of your plugins are up to date.";
-$l['success_plugin_activated'] = "The selected plugin has been activated successfully.";
-$l['success_plugin_deactivated'] = "The selected plugin has been deactivated successfully.";
-$l['success_plugin_installed'] = "The selected plugin has been installed and activated successfully.";
-$l['success_plugin_uninstalled'] = "The selected plugin has been uninstalled successfully.";
+$l['success_plugins_up_to_date'] = "Συγχαρητήρια, όλα τα πρόσθετά σας είναι ενημερωμένα.";
+$l['success_plugin_activated'] = "Το επιλεγμένο πρόσθετο ενεργοποιήθηκε με επιτυχία.";
+$l['success_plugin_deactivated'] = "Το επιλεγμένο πρόσθετο απενεργοποιήθηκε με επιτυχία.";
+$l['success_plugin_installed'] = "Το επιλεγμένο πρόσθετο εγκαταστάθηκε και ενεργοποιήθηκε με επιτυχία.";
+$l['success_plugin_uninstalled'] = "Το επιλεγμένο πρόσθετο απεγκαταστάθηκε με επιτυχία.";
