65854dced71fb016c92f08aa23db4d09a2e4a2ee
Dimitris Panokostas <midwan@gmail.com>
2024-06-06 17:29:48 +0200

    translated config_questions

--- inc/languages/greek/admin/config_questions.lang.php
+++ inc/languages/greek/admin/config_questions.lang.php
@@ -1,43 +1,43 @@
 <?php
 /**
- * MyBB 1.8 English Language Pack
+ * MyBB 1.8 Greek Language Pack
  * Copyright 2014 MyBB Group, All Rights Reserved
  *
  */
 
-$l['security_questions'] = "Security Questions";
-$l['security_questions_desc'] = "Here you can create and manage security questions that will be randomly shown during registration.";
-$l['add_new_question'] = "Add New Question";
-$l['add_new_question_desc'] = "Add a question and its answers for the security questions to be used during registration.";
-$l['edit_question'] = "Edit Question";
-$l['edit_question_desc'] = "Here you edit a security question.";
-$l['preview_question'] = "Preview Question";
+$l['security_questions'] = "Ερωτήσεις Ασφαλείας";
+$l['security_questions_desc'] = "Εδώ μπορείτε να δημιουργήσετε και να διαχειριστείτε ερωτήσεις ασφαλείας που θα εμφανίζονται τυχαία κατά την εγγραφή.";
+$l['add_new_question'] = "Προσθήκη Νέας Ερώτησης";
+$l['add_new_question_desc'] = "Προσθέστε μια ερώτηση και τις απαντήσεις της για τις ερωτήσεις ασφαλείας που θα χρησιμοποιηθούν κατά την εγγραφή.";
+$l['edit_question'] = "Επεξεργασία Ερώτησης";
+$l['edit_question_desc'] = "Εδώ μπορείτε να επεξεργαστείτε μια ερώτηση ασφαλείας.";
+$l['preview_question'] = "Προεπισκόπηση Ερώτησης";
 
-$l['options'] = "Options";
-$l['shown'] = "Shown";
-$l['correct'] = "Correct";
-$l['incorrect'] = "Incorrect";
-$l['disable_question'] = "Disable Question";
-$l['enable_question'] = "Enable Question";
-$l['delete_question'] = "Delete Question";
-$l['no_security_questions'] = "There are no security questions on your forum at this time.";
+$l['options'] = "Επιλογές";
+$l['shown'] = "Εμφανίζεται";
+$l['correct'] = "Σωστή";
+$l['incorrect'] = "Λανθασμένη";
+$l['disable_question'] = "Απενεργοποίηση Ερώτησης";
+$l['enable_question'] = "Ενεργοποίηση Ερώτησης";
+$l['delete_question'] = "Διαγραφή Ερώτησης";
+$l['no_security_questions'] = "Δεν υπάρχουν ερωτήσεις ασφαλείας στο φόρουμ σας αυτή τη στιγμή.";
 
-$l['question'] = "Question";
-$l['question_desc'] = "Enter the question you want answered during registration.";
-$l['answers'] = "Answer(s)";
-$l['answers_desc'] = "Enter the answer(s) to the question above. Please enter each answer on a separate line.";
-$l['active'] = "Active";
-$l['save_question'] = "Save Question";
+$l['question'] = "Ερώτηση";
+$l['question_desc'] = "Εισαγάγετε την ερώτηση που θέλετε να απαντηθεί κατά την εγγραφή.";
+$l['answers'] = "Απαντήσεις";
+$l['answers_desc'] = "Εισαγάγετε τις απαντήσεις στην παραπάνω ερώτηση. Παρακαλούμε εισαγάγετε κάθε απάντηση σε ξεχωριστή γραμμή.";
+$l['active'] = "Ενεργή";
+$l['save_question'] = "Αποθήκευση Ερώτησης";
 
-$l['error_invalid_question'] = "You have selected an invalid question.";
-$l['error_missing_question'] = "You did not enter a question for this question";
-$l['error_missing_answer'] = "You did not enter any answers for this question";
+$l['error_invalid_question'] = "Έχετε επιλέξει μια μη έγκυρη ερώτηση.";
+$l['error_missing_question'] = "Δεν εισαγάγατε μια ερώτηση για αυτή την ερώτηση";
+$l['error_missing_answer'] = "Δεν εισαγάγατε καμία απάντηση για αυτή την ερώτηση";
 
-$l['success_question_created'] = "The question has been created successfully.";
-$l['success_question_updated'] = "The question has been updated successfully.";
-$l['success_question_disabled'] = "The question has been disabled successfully.";
-$l['success_question_enabled'] = "The question has been enabled successfully.";
-$l['success_question_deleted'] = "The question has been deleted successfully.";
+$l['success_question_created'] = "Η ερώτηση δημιουργήθηκε με επιτυχία.";
+$l['success_question_updated'] = "Η ερώτηση ενημερώθηκε με επιτυχία.";
+$l['success_question_disabled'] = "Η ερώτηση απενεργοποιήθηκε με επιτυχία.";
+$l['success_question_enabled'] = "Η ερώτηση ενεργοποιήθηκε με επιτυχία.";
+$l['success_question_deleted'] = "Η ερώτηση διαγράφηκε με επιτυχία.";
 
-$l['confirm_question_deletion'] = "Are you sure you wish to delete this question?";
+$l['confirm_question_deletion'] = "Είστε βέβαιοι ότι θέλετε να διαγράψετε αυτήν την ερώτηση;";
 
