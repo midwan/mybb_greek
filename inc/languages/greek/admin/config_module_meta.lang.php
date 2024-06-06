222811fc0255dea5df96bffa290d822e898cb103
Dimitris Panokostas <midwan@gmail.com>
2024-06-06 17:23:56 +0200

    translated config_module_meta

--- inc/languages/greek/admin/config_module_meta.lang.php
+++ inc/languages/greek/admin/config_module_meta.lang.php
@@ -1,47 +1,47 @@
 <?php
 /**
- * MyBB 1.8 English Language Pack
+ * MyBB 1.8 Greek Language Pack
  * Copyright 2014 MyBB Group, All Rights Reserved
  *
  */
 
-$l['configuration'] = "Configuration";
+$l['configuration'] = "Ρυθμίσεις";
 
-$l['bbsettings'] = "Settings";
-$l['banning'] = "Banning";
-$l['custom_profile_fields'] = "Custom Profile Fields";
-$l['smilies'] = "Smilies";
-$l['word_filters'] = "Word Filters";
+$l['bbsettings'] = "Ρυθμίσεις";
+$l['banning'] = "Αποκλεισμός";
+$l['custom_profile_fields'] = "Προσαρμοσμένα πεδία προφίλ";
+$l['smilies'] = "Συναισθηματικά";
+$l['word_filters'] = "Φίλτρα λέξεων";
 $l['mycode'] = "MyCode";
-$l['languages'] = "Languages";
-$l['post_icons'] = "Post Icons";
-$l['help_documents'] = "Help Documents";
-$l['plugins'] = "Plugins";
-$l['attachment_types'] = "Attachment Types";
-$l['moderator_tools'] = "Moderator Tools";
-$l['spiders_bots'] = "Spiders / Bots";
-$l['calendars'] = "Calendars";
-$l['warning_system'] = "Warning System";
-$l['thread_prefixes'] = "Thread Prefixes";
-$l['security_questions'] = "Security Questions";
-$l['report_reasons'] = "Report Reasons";
+$l['languages'] = "Γλώσσες";
+$l['post_icons'] = "Εικονίδια δημοσιεύσεων";
+$l['help_documents'] = "Έγγραφα βοήθειας";
+$l['plugins'] = "Πρόσθετα";
+$l['attachment_types'] = "Τύποι συνημμένων";
+$l['moderator_tools'] = "Εργαλεία διαχειριστή";
+$l['spiders_bots'] = "Αράχνες / Ρομπότ";
+$l['calendars'] = "Ημερολόγια";
+$l['warning_system'] = "Σύστημα προειδοποίησης";
+$l['thread_prefixes'] = "Προθέματα θέματος";
+$l['security_questions'] = "Ερωτήσεις ασφαλείας";
+$l['report_reasons'] = "Λόγοι αναφοράς";
 
-$l['can_manage_settings'] = "Can manage settings?";
-$l['can_manage_banned_accounts'] = "Can manage banned accounts?";
-$l['can_manage_custom_profile_fields'] = "Can manage custom profile fields?";
-$l['can_manage_smilies'] = "Can manage smilies?";
-$l['can_manage_bad_words'] = "Can manage word filters?";
-$l['can_manage_custom_mycode'] = "Can manage custom MyCode?";
-$l['can_manage_language_packs'] = "Can manage language packs?";
-$l['can_manage_post_icons'] = "Can manage post icons?";
-$l['can_manage_help_documents'] = "Can manage help documents?";
-$l['can_manage_plugins'] = "Can manage plugins?";
-$l['can_manage_attachment_types'] = "Can manage attachment types?";
-$l['can_manage_spiders_bots'] = "Can manage spiders / bots?";
-$l['can_manage_calendars'] = "Can manage calendars?";
-$l['can_manage_warning_system'] = "Can manage warning system?";
-$l['can_manage_mod_tools'] = "Can manage moderator tools?";
-$l['can_manage_thread_prefixes'] = "Can manage thread prefixes?";
-$l['can_manage_security_questions'] = "Can manage security questions?";
-$l['can_manage_report_reasons'] = "Can manage report reasons?";
+$l['can_manage_settings'] = "Μπορεί να διαχειριστεί τις ρυθμίσεις;";
+$l['can_manage_banned_accounts'] = "Μπορεί να διαχειριστεί αποκλεισμένους λογαριασμούς;";
+$l['can_manage_custom_profile_fields'] = "Μπορεί να διαχειριστεί προσαρμοσμένα πεδία προφίλ;";
+$l['can_manage_smilies'] = "Μπορεί να διαχειριστεί συναισθηματικά;";
+$l['can_manage_bad_words'] = "Μπορεί να διαχειριστεί φίλτρα λέξεων;";
+$l['can_manage_custom_mycode'] = "Μπορεί να διαχειριστεί προσαρμοσμένο MyCode;";
+$l['can_manage_language_packs'] = "Μπορεί να διαχειριστεί πακέτα γλώσσας;";
+$l['can_manage_post_icons'] = "Μπορεί να διαχειριστεί εικονίδια δημοσιεύσεων;";
+$l['can_manage_help_documents'] = "Μπορεί να διαχειριστεί έγγραφα βοήθειας;";
+$l['can_manage_plugins'] = "Μπορεί να διαχειριστεί πρόσθετα;";
+$l['can_manage_attachment_types'] = "Μπορεί να διαχειριστεί τύπους συνημμένων;";
+$l['can_manage_spiders_bots'] = "Μπορεί να διαχειριστεί αράχνες / ρομπότ;";
+$l['can_manage_calendars'] = "Μπορεί να διαχειριστεί ημερολόγια;";
+$l['can_manage_warning_system'] = "Μπορεί να διαχειριστεί το σύστημα προειδοποίησης;";
+$l['can_manage_mod_tools'] = "Μπορεί να διαχειριστεί εργαλεία διαχειριστή;";
+$l['can_manage_thread_prefixes'] = "Μπορεί να διαχειριστεί προθέματα θέματος;";
+$l['can_manage_security_questions'] = "Μπορεί να διαχειριστεί ερωτήσεις ασφαλείας;";
+$l['can_manage_report_reasons'] = "Μπορεί να διαχειριστεί λόγους αναφοράς;";
 
