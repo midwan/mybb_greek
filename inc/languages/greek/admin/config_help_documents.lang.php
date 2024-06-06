a0490b4f3c793f245ecb708e3f3c8fc0b3e0c36a
Dimitris Panokostas <midwan@gmail.com>
2024-06-06 17:17:45 +0200

    translated config_help_documents

--- inc/languages/greek/admin/config_help_documents.lang.php
+++ inc/languages/greek/admin/config_help_documents.lang.php
@@ -1,60 +1,60 @@
 <?php
 /**
- * MyBB 1.8 English Language Pack
+ * MyBB 1.8 Greek Language Pack
  * Copyright 2014 MyBB Group, All Rights Reserved
  *
  */
 
-$l['help_documents'] = "Help Documents";
-$l['add_new_section'] = "Add New Section";
-$l['add_new_section_desc'] = "Here you can add a new help section.";
-$l['add_new_document'] = "Add New Document";
-$l['add_new_document_desc'] = "Here you can add a new help document.";
-$l['edit_section'] = "Edit Section";
-$l['edit_section_desc'] = "Here you can edit a help section.";
-$l['edit_document'] = "Edit Document";
-$l['edit_document_desc'] = "Here you can edit a help document.";
-$l['manage_help_documents'] = "Manage Help Documents";
-$l['manage_help_documents_desc'] = "This section allows you to manage your help documents.";
+$l['help_documents'] = "Έγγραφα Βοήθειας";
+$l['add_new_section'] = "Προσθήκη Νέας Ενότητας";
+$l['add_new_section_desc'] = "Εδώ μπορείτε να προσθέσετε μια νέα ενότητα βοήθειας.";
+$l['add_new_document'] = "Προσθήκη Νέου Εγγράφου";
+$l['add_new_document_desc'] = "Εδώ μπορείτε να προσθέσετε ένα νέο έγγραφο βοήθειας.";
+$l['edit_section'] = "Επεξεργασία Ενότητας";
+$l['edit_section_desc'] = "Εδώ μπορείτε να επεξεργαστείτε μια ενότητα βοήθειας.";
+$l['edit_document'] = "Επεξεργασία Εγγράφου";
+$l['edit_document_desc'] = "Εδώ μπορείτε να επεξεργαστείτε ένα έγγραφο βοήθειας.";
+$l['manage_help_documents'] = "Διαχείριση Εγγράφων Βοήθειας";
+$l['manage_help_documents_desc'] = "Αυτή η ενότητα σας επιτρέπει να διαχειρίζεστε τα έγγραφα βοήθειας σας.";
 
-$l['title'] = "Title";
-$l['short_description'] = "Short Description";
-$l['display_order'] = "Display Order";
-$l['enabled'] = "Enabled?";
-$l['use_translation'] = "Use Translation?";
-$l['add_section'] = "Add Section";
-$l['add_document'] = "Add Document";
-$l['save_section'] = "Save Section";
-$l['save_document'] = "Save Document";
-$l['section'] = "Section";
-$l['document'] = "Document";
+$l['title'] = "Τίτλος";
+$l['short_description'] = "Σύντομη Περιγραφή";
+$l['display_order'] = "Σειρά Εμφάνισης";
+$l['enabled'] = "Ενεργοποιημένο;";
+$l['use_translation'] = "Χρήση Μετάφρασης;";
+$l['add_section'] = "Προσθήκη Ενότητας";
+$l['add_document'] = "Προσθήκη Εγγράφου";
+$l['save_section'] = "Αποθήκευση Ενότητας";
+$l['save_document'] = "Αποθήκευση Εγγράφου";
+$l['section'] = "Ενότητα";
+$l['document'] = "Έγγραφο";
 $l['id'] = "ID";
-$l['custom_doc_sec'] = "Custom Document/Section";
-$l['default_doc_sec'] = "Default Document/Section";
-$l['no_help_documents'] = "There are no help documents on your forum at this time.";
-$l['section_document'] = "Section / Document";
+$l['custom_doc_sec'] = "Προσαρμοσμένο Έγγραφο/Ενότητα";
+$l['default_doc_sec'] = "Προεπιλεγμένο Έγγραφο/Ενότητα";
+$l['no_help_documents'] = "Δεν υπάρχουν έγγραφα βοήθειας στο φόρουμ σας αυτή τη στιγμή.";
+$l['section_document'] = "Ενότητα / Έγγραφο";
 
-$l['error_section_missing_name'] = "You must specify a name for the section.";
-$l['error_section_missing_description'] = "You must specify a short description for the section.";
-$l['error_section_missing_enabled'] = "You must specify yes or no for \"Enabled?\".";
-$l['error_section_missing_translation'] = "You must specify yes or no for \"Use Translation?\".";
-$l['error_missing_sid'] = "You must specify a section for the document.";
-$l['error_document_missing_name'] = "You must specify a name for the document.";
-$l['error_document_missing_description'] = "You must specify a short description for the document.";
-$l['error_document_missing_document'] = "You must specify the document.";
-$l['error_document_missing_enabled'] = "You must specify yes or no for \"Enabled?\".";
-$l['error_document_missing_translation'] = "You must specify yes or no for \"Use Translation?\".";
-$l['error_invalid_sid'] = "Invalid document id specified.";
-$l['error_missing_section_id'] = "The specified section does not exist.";
-$l['error_missing_hid'] = "The specified document does not exist.";
+$l['error_section_missing_name'] = "Πρέπει να καθορίσετε ένα όνομα για την ενότητα.";
+$l['error_section_missing_description'] = "Πρέπει να καθορίσετε μια σύντομη περιγραφή για την ενότητα.";
+$l['error_section_missing_enabled'] = "Πρέπει να καθορίσετε ναι ή όχι για το \"Ενεργοποιημένο;\".";
+$l['error_section_missing_translation'] = "Πρέπει να καθορίσετε ναι ή όχι για το \"Χρήση Μετάφρασης;\".";
+$l['error_missing_sid'] = "Πρέπει να καθορίσετε μια ενότητα για το έγγραφο.";
+$l['error_document_missing_name'] = "Πρέπει να καθορίσετε ένα όνομα για το έγγραφο.";
+$l['error_document_missing_description'] = "Πρέπει να καθορίσετε μια σύντομη περιγραφή για το έγγραφο.";
+$l['error_document_missing_document'] = "Πρέπει να καθορίσετε το έγγραφο.";
+$l['error_document_missing_enabled'] = "Πρέπει να καθορίσετε ναι ή όχι για το \"Ενεργοποιημένο;\".";
+$l['error_document_missing_translation'] = "Πρέπει να καθορίσετε ναι ή όχι για το \"Χρήση Μετάφρασης;\".";
+$l['error_invalid_sid'] = "Καθορίστηκε μη έγκυρο αναγνωριστικό εγγράφου.";
+$l['error_missing_section_id'] = "Η καθορισμένη ενότητα δεν υπάρχει.";
+$l['error_missing_hid'] = "Το καθορισμένο έγγραφο δεν υπάρχει.";
 
-$l['success_help_section_added'] = "The help section has been added successfully.";
-$l['success_help_document_added'] = "The help document has been added successfully.";
-$l['success_help_section_updated'] = "The help section has been updated successfully.";
-$l['success_help_document_updated'] = "The help document has been updated successfully.";
-$l['success_section_deleted'] = "The selected help section has been deleted successfully.";
-$l['success_document_deleted'] = "The selected help document has been deleted successfully.";
+$l['success_help_section_added'] = "Η ενότητα βοήθειας προστέθηκε με επιτυχία.";
+$l['success_help_document_added'] = "Το έγγραφο βοήθειας προστέθηκε με επιτυχία.";
+$l['success_help_section_updated'] = "Η ενότητα βοήθειας ενημερώθηκε με επιτυχία.";
+$l['success_help_document_updated'] = "Το έγγραφο βοήθειας ενημερώθηκε με επιτυχία.";
+$l['success_section_deleted'] = "Η επιλεγμένη ενότητα βοήθειας διαγράφηκε με επιτυχία.";
+$l['success_document_deleted'] = "Το επιλεγμένο έγγραφο βοήθειας διαγράφηκε με επιτυχία.";
 
-$l['confirm_section_deletion'] = "Are you sure you wish to delete this section?";
-$l['confirm_document_deletion'] = "Are you sure you wish to delete this document?";
+$l['confirm_section_deletion'] = "Είστε βέβαιοι ότι θέλετε να διαγράψετε αυτήν την ενότητα;";
+$l['confirm_document_deletion'] = "Είστε βέβαιοι ότι θέλετε να διαγράψετε αυτό το έγγραφο;";
 
