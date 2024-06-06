71a7b052394594ff18f54a073528e6f750c542a7
Dimitris Panokostas <midwan@gmail.com>
2024-06-06 17:25:25 +0200

    translated config_mycode

--- inc/languages/greek/admin/config_mycode.lang.php
+++ inc/languages/greek/admin/config_mycode.lang.php
@@ -1,59 +1,59 @@
 <?php
 /**
- * MyBB 1.8 English Language Pack
+ * MyBB 1.8 Greek Language Pack
  * Copyright 2014 MyBB Group, All Rights Reserved
  *
  */
 
-$l['custom_mycode'] = "Custom MyCode";
+$l['custom_mycode'] = "Προσαρμοσμένο MyCode";
 $l['mycode'] = "MyCode";
-$l['mycode_desc'] = "Custom MyCode allows you to create additional MyCode for your members to use in their messages.";
-$l['add_new_mycode'] = "Add New MyCode";
-$l['add_new_mycode_desc'] = "Here you can add a new custom MyCode to your forum. You can use the MyCode Sandbox below the form to test your regular expression and replacement before saving.";
-$l['edit_mycode_desc'] = "Here you can edit a custom MyCode. You can use the MyCode Sandbox below the form to test your regular expression and replacement before saving your changes.";
+$l['mycode_desc'] = "Το προσαρμοσμένο MyCode σας επιτρέπει να δημιουργήσετε επιπλέον MyCode για τα μέλη σας να χρησιμοποιούν στα μηνύματά τους.";
+$l['add_new_mycode'] = "Προσθήκη Νέου MyCode";
+$l['add_new_mycode_desc'] = "Εδώ μπορείτε να προσθέσετε ένα νέο προσαρμοσμένο MyCode στο φόρουμ σας. Μπορείτε να χρησιμοποιήσετε το MyCode Sandbox κάτω από τη φόρμα για να δοκιμάσετε την κανονική έκφραση και την αντικατάσταση πριν αποθηκεύσετε.";
+$l['edit_mycode_desc'] = "Εδώ μπορείτε να επεξεργαστείτε ένα προσαρμοσμένο MyCode. Μπορείτε να χρησιμοποιήσετε το MyCode Sandbox κάτω από τη φόρμα για να δοκιμάσετε την κανονική έκφραση και την αντικατάσταση πριν αποθηκεύσετε τις αλλαγές σας.";
 
-$l['title'] = "Title";
-$l['short_description'] = "Short Description";
-$l['regular_expression'] = "Regular Expression";
-$l['regular_expression_desc'] = "Enter a regular expression that will search for a specific combination of characters.  You must make sure the regular expression is valid and safe&mdash;no validation is performed.";
-$l['replacement'] = "Replacement";
-$l['replacement_desc'] = "Enter a replacement for the regular expression.";
-$l['example'] = "Example:";
-$l['enabled'] = "Enabled?";
-$l['parse_order'] = "Parse Order";
-$l['parse_order_desc'] = "MyCodes will be parsed in ascending order relative to other MyCodes.";
-$l['edit_mycode'] = "Edit MyCode";
-$l['activate_mycode'] = "Activate MyCode";
-$l['deactivate_mycode'] = "Deactivate MyCode";
-$l['delete_mycode'] = "Delete MyCode";
-$l['no_mycode'] = "No custom MyCodes exist at this time.";
-$l['save_mycode'] = "Save MyCode";
-$l['add_mycode'] = "Add MyCode";
-$l['changes_not_saved'] = "Your changes have not yet been saved.";
+$l['title'] = "Τίτλος";
+$l['short_description'] = "Σύντομη Περιγραφή";
+$l['regular_expression'] = "Κανονική Έκφραση";
+$l['regular_expression_desc'] = "Εισαγάγετε μια κανονική έκφραση που θα αναζητήσει μια συγκεκριμένη συνδυασμό χαρακτήρων. Πρέπει να βεβαιωθείτε ότι η κανονική έκφραση είναι έγκυρη και ασφαλής - δεν γίνεται καμία επικύρωση.";
+$l['replacement'] = "Αντικατάσταση";
+$l['replacement_desc'] = "Εισαγάγετε μια αντικατάσταση για την κανονική έκφραση.";
+$l['example'] = "Παράδειγμα:";
+$l['enabled'] = "Ενεργοποιημένο;";
+$l['parse_order'] = "Σειρά Ανάλυσης";
+$l['parse_order_desc'] = "Τα MyCodes θα αναλυθούν με αύξουσα σειρά σε σχέση με άλλα MyCodes.";
+$l['edit_mycode'] = "Επεξεργασία MyCode";
+$l['activate_mycode'] = "Ενεργοποίηση MyCode";
+$l['deactivate_mycode'] = "Απενεργοποίηση MyCode";
+$l['delete_mycode'] = "Διαγραφή MyCode";
+$l['no_mycode'] = "Δεν υπάρχουν προσαρμοσμένα MyCodes αυτή τη στιγμή.";
+$l['save_mycode'] = "Αποθήκευση MyCode";
+$l['add_mycode'] = "Προσθήκη MyCode";
+$l['changes_not_saved'] = "Οι αλλαγές σας δεν έχουν αποθηκευτεί ακόμη.";
 
 $l['sandbox'] = "Sandbox";
-$l['sandbox_desc'] = "You can use this area to test the regular expression and replacement above before saving your changes.";
-$l['test_value'] = "Test Value";
-$l['test_value_desc'] = "Enter in text to be tested in the box below.";
-$l['result_html'] = "HTML Result";
-$l['result_html_desc'] = "The text area below shows the resulting HTML using the regular expression on the test value.";
-$l['result_actual'] = "Actual Result";
-$l['result_actual_desc'] = "The area below shows the actual result when the HTML is rendered.";
-$l['test'] = "Test MyCode";
+$l['sandbox_desc'] = "Μπορείτε να χρησιμοποιήσετε αυτήν την περιοχή για να δοκιμάσετε την κανονική έκφραση και την αντικατάσταση παραπάνω πριν αποθηκεύσετε τις αλλαγές σας.";
+$l['test_value'] = "Τιμή Δοκιμής";
+$l['test_value_desc'] = "Εισαγάγετε κείμενο για να δοκιμάσετε στο παρακάτω πλαίσιο.";
+$l['result_html'] = "Αποτέλεσμα HTML";
+$l['result_html_desc'] = "Το πεδίο κειμένου παρακάτω εμφανίζει το αποτέλεσμα HTML χρησιμοποιώντας την κανονική έκφραση στην τιμή δοκιμής.";
+$l['result_actual'] = "Πραγματικό Αποτέλεσμα";
+$l['result_actual_desc'] = "Η περιοχή παρακάτω εμφανίζει το πραγματικό αποτέλεσμα όταν το HTML αποδίδεται.";
+$l['test'] = "Δοκιμή MyCode";
 
-$l['error_missing_title'] = "You did not enter a title.";
-$l['error_missing_regex'] = "You did not enter a regular expression.";
-$l['error_regex_already_available'] = "You entered a regular expression which is already defined in another MyCode. Please provide a different regular expression for this MyCode.";
-$l['error_missing_replacement'] = "You did not enter a replacement.";
-$l['error_invalid_mycode'] = "The specified MyCode does not exist.";
+$l['error_missing_title'] = "Δεν εισαγάγατε τίτλο.";
+$l['error_missing_regex'] = "Δεν εισαγάγατε κανονική έκφραση.";
+$l['error_regex_already_available'] = "Εισαγάγατε μια κανονική έκφραση που έχει ήδη καθοριστεί σε άλλο MyCode. Παρακαλούμε δώστε μια διαφορετική κανονική έκφραση για αυτό το MyCode.";
+$l['error_missing_replacement'] = "Δεν εισαγάγατε αντικατάσταση.";
+$l['error_invalid_mycode'] = "Το καθορισμένο MyCode δεν υπάρχει.";
 
-$l['success_added_mycode'] = "The MyCode has been added successfully.";
-$l['success_deleted_mycode'] = "The selected MyCode has been deleted successfully.";
-$l['success_updated_mycode'] = "The MyCode has been updated successfully.";
-$l['success_deactivated_mycode'] = "The selected MyCode has been deactivated successfully.";
-$l['success_activated_mycode'] = "The selected MyCode has been activated successfully.";
+$l['success_added_mycode'] = "Το MyCode προστέθηκε με επιτυχία.";
+$l['success_deleted_mycode'] = "Το επιλεγμένο MyCode διαγράφηκε με επιτυχία.";
+$l['success_updated_mycode'] = "Το MyCode ενημερώθηκε με επιτυχία.";
+$l['success_deactivated_mycode'] = "Το επιλεγμένο MyCode απενεργοποιήθηκε με επιτυχία.";
+$l['success_activated_mycode'] = "Το επιλεγμένο MyCode ενεργοποιήθηκε με επιτυχία.";
 
-$l['confirm_mycode_deletion'] = "Are you sure you wish to delete this MyCode?";
+$l['confirm_mycode_deletion'] = "Είστε βέβαιοι ότι θέλετε να διαγράψετε αυτό το MyCode;";
 
-$l['mycode_sandbox_test_error'] = 'There was an error fetching the test results.';
+$l['mycode_sandbox_test_error'] = 'Παρουσιάστηκε σφάλμα κατά την ανάκτηση των αποτελεσμάτων δοκιμής.';
 
