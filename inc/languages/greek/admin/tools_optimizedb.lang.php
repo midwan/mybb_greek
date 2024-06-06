2d50e07b94442c00c4bcc734173ca48533bff1d2
Dimitris Panokostas <midwan@gmail.com>
2024-06-06 18:53:08 +0200

    translated tools_optimizedb

--- inc/languages/greek/admin/tools_optimizedb.lang.php
+++ inc/languages/greek/admin/tools_optimizedb.lang.php
@@ -1,20 +1,19 @@
 <?php
 /**
- * MyBB 1.8 English Language Pack
+ * MyBB 1.8 Greek Language Pack
  * Copyright 2014 MyBB Group, All Rights Reserved
  *
  */
 
-$l['optimize_database'] = "Optimize Database";
+$l['optimize_database'] = "Βελτιστοποίηση Βάσης Δεδομένων";
 
-$l['table_selection'] = "Table Selection";
-$l['tables_select_desc'] = "You may select the database tables you wish to perform this action on here. Hold down CTRL to select multiple tables.";
-$l['select_all'] = "Select All";
-$l['deselect_all'] = "Deselect All";
-$l['select_forum_tables'] = "Select Forum Tables";
-$l['optimize_selected_tables'] = "Optimize Selected Tables";
+$l['table_selection'] = "Επιλογή Πινάκων";
+$l['tables_select_desc'] = "Μπορείτε να επιλέξετε τους πίνακες της βάσης δεδομένων που θέλετε να εκτελέσετε αυτήν την ενέργεια εδώ. Κρατήστε πατημένο το πλήκτρο CTRL για να επιλέξετε πολλαπλούς πίνακες.";
+$l['select_all'] = "Επιλογή Όλων";
+$l['deselect_all'] = "Αποεπιλογή Όλων";
+$l['select_forum_tables'] = "Επιλογή Πινάκων Φόρουμ";
+$l['optimize_selected_tables'] = "Βελτιστοποίηση Επιλεγμένων Πινάκων";
 
-$l['error_no_tables_selected'] = "You did not select any database tables to optimize.";
+$l['error_no_tables_selected'] = "Δεν επιλέξατε κανέναν πίνακα βάσης δεδομένων για βελτιστοποίηση.";
 
-$l['success_tables_optimized'] = "The selected tables have been optimized and analyzed successfully.";
-
+$l['success_tables_optimized'] = "Οι επιλεγμένοι πίνακες έχουν βελτιστοποιηθεί και αναλυθεί με επιτυχία.";
