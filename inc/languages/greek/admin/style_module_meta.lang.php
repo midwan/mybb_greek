93a617d531bf5e03dfe9d67076b59660019e0fcb
Dimitris Panokostas <midwan@gmail.com>
2024-06-06 18:20:22 +0200

    translated style_module_meta

--- inc/languages/greek/admin/style_module_meta.lang.php
+++ inc/languages/greek/admin/style_module_meta.lang.php
@@ -1,15 +1,15 @@
 <?php
 /**
- * MyBB 1.8 English Language Pack
+ * MyBB 1.8 Greek Language Pack
  * Copyright 2014 MyBB Group, All Rights Reserved
  *
  */
 
-$l['templates_and_style'] = "Templates &amp; Style";
+$l['templates_and_style'] = "Πρότυπα &amp; Στυλ";
 
-$l['themes'] = "Themes";
-$l['templates'] = "Templates";
+$l['themes'] = "Θέματα";
+$l['templates'] = "Πρότυπα";
 
-$l['can_manage_themes'] = "Can manage themes?";
-$l['can_manage_templates'] = "Can manage templates?";
+$l['can_manage_themes'] = "Μπορεί να διαχειριστεί θέματα;";
+$l['can_manage_templates'] = "Μπορεί να διαχειριστεί πρότυπα;";
 
