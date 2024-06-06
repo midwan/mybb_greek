5ae76cf4db3d190d1c776ad13a5d091ce4907d40
Dimitris Panokostas <midwan@gmail.com>
2024-06-06 19:00:36 +0200

    translated user_admin_permissions

--- inc/languages/greek/admin/user_admin_permissions.lang.php
+++ inc/languages/greek/admin/user_admin_permissions.lang.php
@@ -1,46 +1,46 @@
 <?php
 /**
- * MyBB 1.8 English Language Pack
+ * MyBB 1.8 Greek Language Pack
  * Copyright 2014 MyBB Group, All Rights Reserved
  *
  */
 
-$l['admin_permissions'] = "Admin Permissions";
-$l['user_permissions'] = "User Permissions";
-$l['user_permissions_desc'] = "Here you can manage the administrator permissions for individual users. This effectively allows you to lock certain administrators out of different areas of the Admin CP.";
-$l['group_permissions'] = "Group Permissions";
-$l['group_permissions_desc'] = "Administrator permissions can also be applied to user groups that have permission to access the Admin CP. Similarly you can use this tool to lock out entire administrative groups from accessing the different areas of the Admin CP.";
-$l['default_permissions'] = "Default Permissions";
-$l['default_permissions_desc'] = "The default administrative permissions are those applied to users who do not have custom administrator permissions set for them or are not inheriting group administrator permissions.";
+$l['admin_permissions'] = "Δικαιώματα Διαχειριστή";
+$l['user_permissions'] = "Δικαιώματα Χρήστη";
+$l['user_permissions_desc'] = "Εδώ μπορείτε να διαχειριστείτε τα δικαιώματα διαχειριστή για τους μεμονωμένους χρήστες. Αυτό σας επιτρέπει να κλειδώσετε ορισμένους διαχειριστές από διάφορες περιοχές του Πίνακα Διαχείρισης.";
+$l['group_permissions'] = "Δικαιώματα Ομάδας";
+$l['group_permissions_desc'] = "Τα δικαιώματα διαχειριστή μπορούν επίσης να εφαρμοστούν σε ομάδες χρηστών που έχουν άδεια πρόσβασης στον Πίνακα Διαχείρισης. Με αυτό το εργαλείο μπορείτε επίσης να κλειδώσετε ολόκληρες διαχειριστικές ομάδες από την πρόσβαση στις διάφορες περιοχές του Πίνακα Διαχείρισης.";
+$l['default_permissions'] = "Προεπιλεγμένα Δικαιώματα";
+$l['default_permissions_desc'] = "Τα προεπιλεγμένα δικαιώματα διαχειριστή είναι αυτά που εφαρμόζονται στους χρήστες που δεν έχουν καθορισμένα προσαρμοσμένα δικαιώματα διαχειριστή ή δεν κληρονομούν δικαιώματα διαχειριστή από ομάδες.";
 
-$l['admin_permissions_updated'] = "The admin permissions have been updated successfully.";
-$l['revoke_permissions'] = "Revoke Permissions";
-$l['edit_permissions'] = "Edit Permissions";
-$l['set_permissions'] = "Set Permissions";
-$l['edit_permissions_desc'] = "Here you can restrict access to entire tabs or individual pages. Be aware that the \"Home\" tab is accessible to all administrators.";
-$l['update_permissions'] = "Update Permissions";
-$l['view_log'] = "View Log";
-$l['permissions_type_group'] = "Permission type of the group";
-$l['permissions_type_user'] = "Permission type of the user";
-$l['no_group_perms'] = "There are currently no set group permissions.";
-$l['no_user_perms'] = "There are currently no set user permissions.";
-$l['edit_user'] = "Edit User Profile";
-$l['using_individual_perms'] = "Using Individual Permissions";
-$l['using_custom_perms'] = "Using Custom Permissions";
-$l['using_group_perms'] = "Using Group Permissions";
-$l['using_default_perms'] = "Using Default Permissions";
-$l['last_active'] = "Last Active";
-$l['user'] = "User";
-$l['edit_group'] = "Edit Group";
-$l['default'] = "Default";
-$l['group'] = "Group";
+$l['admin_permissions_updated'] = "Τα δικαιώματα διαχειριστή ενημερώθηκαν με επιτυχία.";
+$l['revoke_permissions'] = "Ανάκληση Δικαιωμάτων";
+$l['edit_permissions'] = "Επεξεργασία Δικαιωμάτων";
+$l['set_permissions'] = "Ορισμός Δικαιωμάτων";
+$l['edit_permissions_desc'] = "Εδώ μπορείτε να περιορίσετε την πρόσβαση σε ολόκληρες καρτέλες ή μεμονωμένες σελίδες. Προσέξτε ότι η καρτέλα \"Αρχική\" είναι προσβάσιμη σε όλους τους διαχειριστές.";
+$l['update_permissions'] = "Ενημέρωση Δικαιωμάτων";
+$l['view_log'] = "Προβολή Καταγραφής";
+$l['permissions_type_group'] = "Τύπος δικαιωμάτων της ομάδας";
+$l['permissions_type_user'] = "Τύπος δικαιωμάτων του χρήστη";
+$l['no_group_perms'] = "Δεν υπάρχουν καθορισμένα δικαιώματα ομάδας αυτή τη στιγμή.";
+$l['no_user_perms'] = "Δεν υπάρχουν καθορισμένα δικαιώματα χρήστη αυτή τη στιγμή.";
+$l['edit_user'] = "Επεξεργασία Προφίλ Χρήστη";
+$l['using_individual_perms'] = "Χρήση Μεμονωμένων Δικαιωμάτων";
+$l['using_custom_perms'] = "Χρήση Προσαρμοσμένων Δικαιωμάτων";
+$l['using_group_perms'] = "Χρήση Δικαιωμάτων Ομάδας";
+$l['using_default_perms'] = "Χρήση Προεπιλεγμένων Δικαιωμάτων";
+$l['last_active'] = "Τελευταία Δραστηριότητα";
+$l['user'] = "Χρήστης";
+$l['edit_group'] = "Επεξεργασία Ομάδας";
+$l['default'] = "Προεπιλογή";
+$l['group'] = "Ομάδα";
 
-$l['error_super_admin'] = 'Sorry, but you cannot perform this action on the specified user as they are a super administrator.';
-$l['error_delete_no_uid'] = 'You did not enter a admin user/usergroup permission id';
-$l['error_delete_invalid_uid'] = 'You did not enter a valid admin user/usergroup permission id';
+$l['error_super_admin'] = 'Λυπούμαστε, αλλά δεν μπορείτε να εκτελέσετε αυτήν την ενέργεια για τον συγκεκριμένο χρήστη, καθώς είναι υπερδιαχειριστής.';
+$l['error_delete_no_uid'] = 'Δεν καταχωρήσατε αριθμό αναγνωριστικού δικαιωμάτων διαχειριστή χρήστη/ομάδας χρηστών';
+$l['error_delete_invalid_uid'] = 'Δεν καταχωρήσατε έγκυρο αριθμό αναγνωριστικού δικαιωμάτων διαχειριστή χρήστη/ομάδας χρηστών';
 
-$l['success_perms_deleted'] = 'The admin user/usergroup permissions has been revoked successfully.';
+$l['success_perms_deleted'] = 'Τα δικαιώματα διαχειριστή χρήστη/ομάδας χρηστών ανακλήθηκαν με επιτυχία.';
 
-$l['confirm_perms_deletion'] = "Are you sure you wish to revoked this admin user/usergroup permissions?";
-$l['confirm_perms_deletion2'] = "Are you sure you wish to revoke this user\'s permissions?";
-$l['confirm_perms_deletion3'] = "Are you sure you wish to revoke this group\'s permissions?";
+$l['confirm_perms_deletion'] = "Είστε βέβαιοι ότι θέλετε να ανακληθούν αυτά τα δικαιώματα διαχειριστή χρήστη/ομάδας χρηστών;";
+$l['confirm_perms_deletion2'] = "Είστε βέβαιοι ότι θέλετε να ανακληθούν τα δικαιώματα αυτού του χρήστη;";
+$l['confirm_perms_deletion3'] = "Είστε βέβαιοι ότι θέλετε να ανακληθούν τα δικαιώματα αυτής της ομάδας;";
