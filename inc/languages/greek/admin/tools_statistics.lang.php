61544a445c9875c683f3aee1d8ed2ebfc3bcdf5e
Dimitris Panokostas <midwan@gmail.com>
2024-06-06 18:55:09 +0200

    translated tools_statistics

--- inc/languages/greek/admin/tools_statistics.lang.php
+++ inc/languages/greek/admin/tools_statistics.lang.php
@@ -1,27 +1,27 @@
 <?php
 /**
- * MyBB 1.8 English Language Pack
+ * MyBB 1.8 Greek Language Pack
  * Copyright 2014 MyBB Group, All Rights Reserved
  *
  */
 
-$l['statistics'] = "Statistics";
-$l['overall_statistics'] = "Overall Statistics";
-$l['overall_statistics_desc'] = "Here you can view overall statistics on your forum. All times are in UTC.";
+$l['statistics'] = "Στατιστικά";
+$l['overall_statistics'] = "Συνολικά Στατιστικά";
+$l['overall_statistics_desc'] = "Εδώ μπορείτε να δείτε τα συνολικά στατιστικά του φόρουμ σας. Όλες οι ώρες είναι σε UTC.";
 
-$l['date_range'] = "Date Range";
+$l['date_range'] = "Εύρος Ημερομηνιών";
 
-$l['date'] = "Date";
-$l['users'] = "Users";
-$l['threads'] = "Threads";
-$l['posts'] = "Posts";
+$l['date'] = "Ημερομηνία";
+$l['users'] = "Χρήστες";
+$l['threads'] = "Θέματα";
+$l['posts'] = "Δημοσιεύσεις";
 
-$l['from'] = "From";
-$l['to'] = "to";
+$l['from'] = "Από";
+$l['to'] = "έως";
 
-$l['increase'] = "Increase";
-$l['no_change'] = "No Change";
-$l['decrease'] = "Decrease";
+$l['increase'] = "Αύξηση";
+$l['no_change'] = "Χωρίς Αλλαγή";
+$l['decrease'] = "Μείωση";
 
-$l['error_no_results_found_for_criteria'] = "We could not find any results for the date range you provided. Please try a different date range.";
-$l['error_no_statistics_available_yet'] = "We're sorry, there are currently no statistics data available for your forum yet.";
+$l['error_no_results_found_for_criteria'] = "Δεν βρέθηκαν αποτελέσματα για το επιλεγμένο εύρος ημερομηνιών. Παρακαλώ δοκιμάστε ένα διαφορετικό εύρος ημερομηνιών.";
+$l['error_no_statistics_available_yet'] = "Λυπούμαστε, δεν υπάρχουν ακόμα διαθέσιμα στατιστικά δεδομένα για το φόρουμ σας.";
