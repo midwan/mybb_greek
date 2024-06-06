3528ed729a810a72a64750e23889438fd1b373a3
Dimitris Panokostas <midwan@gmail.com>
2024-06-06 18:09:14 +0200

    translated forum_module_meta

--- inc/languages/greek/admin/forum_module_meta.lang.php
+++ inc/languages/greek/admin/forum_module_meta.lang.php
@@ -1,19 +1,18 @@
 <?php
 /**
- * MyBB 1.8 English Language Pack
+ * MyBB 1.8 Greek Language Pack
  * Copyright 2014 MyBB Group, All Rights Reserved
  *
  */
 
-$l['forums_and_posts'] = "Forums &amp; Posts";
+$l['forums_and_posts'] = "Φόρουμ &amp; Δημοσιεύσεις";
 
-$l['forum_management'] = "Forum Management";
-$l['forum_announcements'] = "Forum Announcements";
-$l['moderation_queue'] = "Moderation Queue";
-$l['attachments'] = "Attachments";
+$l['forum_management'] = "Διαχείριση Φόρουμ";
+$l['forum_announcements'] = "Ανακοινώσεις Φόρουμ";
+$l['moderation_queue'] = "Ουρά Ελέγχου";
+$l['attachments'] = "Συνημμένα";
 
-$l['can_manage_forums'] = "Can manage forums?";
-$l['can_manage_forum_announcements'] = "Can manage forum announcements?";
-$l['can_moderate'] = "Can moderate posts, threads, and attachments?";
-$l['can_manage_attachments'] = "Can manage attachments?";
-
+$l['can_manage_forums'] = "Μπορεί να διαχειριστεί φόρουμ;";
+$l['can_manage_forum_announcements'] = "Μπορεί να διαχειριστεί ανακοινώσεις φόρουμ;";
+$l['can_moderate'] = "Μπορεί να ελέγξει δημοσιεύσεις, θέματα και συνημμένα;";
+$l['can_manage_attachments'] = "Μπορεί να διαχειριστεί συνημμένα;";
