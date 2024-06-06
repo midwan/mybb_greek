660d97b10c677886e9fac8d74eb1fd46aa13cd4a
Dimitris Panokostas <midwan@gmail.com>
2024-06-06 17:39:36 +0200

    translated config_smilies

--- inc/languages/greek/admin/config_smilies.lang.php
+++ inc/languages/greek/admin/config_smilies.lang.php
@@ -1,64 +1,63 @@
 <?php
 /**
- * MyBB 1.8 English Language Pack
+ * MyBB 1.8 Greek Language Pack
  * Copyright 2014 MyBB Group, All Rights Reserved
  *
  */
+$l['smilies'] = "Χαμόγελα";
+$l['manage_smilies'] = "Διαχείριση Χαμόγελων";
+$l['manage_smilies_desc'] = "Σε αυτήν την ενότητα μπορείτε να επεξεργαστείτε και να διαγράψετε τα χαμόγελά σας.";
+$l['add_smilie'] = "Προσθήκη Νέου Χαμόγελου";
+$l['add_smilie_desc'] = "Εδώ μπορείτε να προσθέσετε ένα μόνο νέο χαμόγελο.";
+$l['add_multiple_smilies'] = "Προσθήκη Πολλαπλών Χαμόγελων";
+$l['add_multiple_smilies_desc'] = "Εδώ μπορείτε να προσθέσετε πολλαπλά νέα χαμόγελα ταυτόχρονα.";
+$l['edit_smilie'] = "Επεξεργασία Χαμόγελου";
+$l['edit_smilie_desc'] = "Εδώ μπορείτε να επεξεργαστείτε ένα μόνο χαμόγελο.";
+$l['mass_edit'] = "Μαζική Επεξεργασία";
+$l['mass_edit_desc'] = "Εδώ μπορείτε να επεξεργαστείτε εύκολα όλα τα χαμόγελά σας με μία κίνηση.";
 
-$l['smilies'] = "Smilies";
-$l['manage_smilies'] = "Manage Smilies";
-$l['manage_smilies_desc'] = "This section allows you to edit and delete your smilies.";
-$l['add_smilie'] = "Add New Smilie";
-$l['add_smilie_desc'] = "Here you can add a single new smilie.";
-$l['add_multiple_smilies'] = "Add Multiple Smilies";
-$l['add_multiple_smilies_desc'] = "Here you can add multiple new smilies at once.";
-$l['edit_smilie'] = "Edit Smilie";
-$l['edit_smilie_desc'] = "Here you can edit a single smilie.";
-$l['mass_edit'] = "Mass Edit";
-$l['mass_edit_desc'] = "Here you can easily edit all your smilies in one go.";
+$l['no_smilies'] = "Δεν υπάρχουν χαμόγελα στο φόρουμ σας αυτήν τη στιγμή.";
 
-$l['no_smilies'] = "There are no smilies on your forum at this time.";
+$l['image'] = "Εικόνα";
+$l['name'] = "Όνομα";
+$l['text_replace'] = "Κείμενο για Αντικατάσταση";
+$l['text_replace_desc'] = "Αυτό είναι το κείμενο που θα αντικατασταθεί με το χαμόγελο (εισαγάγετε ένα ανά γραμμή).";
+$l['image_path'] = "Διαδρομή Εικόνας";
+$l['image_path_desc'] = "Αυτή είναι η διαδρομή προς την εικόνα του χαμόγελου. Εάν θέλετε να χρησιμοποιήσετε διαφορετικές εικόνες χαμόγελων για διάφορα θέματα, χρησιμοποιήστε το <strong>{theme}</strong> για να αναπαραστήσετε τον κατάλογο εικόνων κάθε θέματος.";
+$l['order'] = "Σειρά";
+$l['display_order'] = "Σειρά Εμφάνισης";
+$l['display_order_desc'] = "Η σειρά στη λίστα των χαμόγελων που θα εμφανίζεται αυτό. Αυτός ο αριθμός δεν πρέπει να είναι ίδιος με άλλο χαμόγελο.";
+$l['mass_edit_show_clickable'] = "Εμφάνιση σε Κλικάρισμα;";
+$l['show_clickable'] = "Εμφάνιση στην λίστα κλικαρίσματος;";
+$l['show_clickable_desc'] = "Θέλετε αυτό το χαμόγελο να εμφανίζεται στην λίστα κλικαρίσματος χαμόγελων στον επεξεργαστή αναρτήσεων;";
+$l['include'] = "Προσθήκη;";
+$l['path_to_images'] = "Διαδρομή προς τις Εικόνες";
+$l['path_to_images_desc'] = "Αυτή είναι η διαδρομή προς τον φάκελο στον οποίο βρίσκονται οι εικόνες.";
+$l['smilie_delete'] = "Διαγραφή;";
+$l['save_smilie'] = "Αποθήκευση Χαμόγελου";
+$l['save_smilies'] = "Αποθήκευση Χαμόγελων";
+$l['show_smilies'] = "Εμφάνιση Χαμόγελων";
+$l['reset'] = "Επαναφορά";
 
-$l['image'] = "Image";
-$l['name'] = "Name";
-$l['text_replace'] = "Text to Replace";
-$l['text_replace_desc'] = "This is the text to replace with the smilie (enter one per line).";
-$l['image_path'] = "Image Path";
-$l['image_path_desc'] = "This is the path to the smilie image. If you want to use different smilie images for different themes, please use <strong>{theme}</strong> to represent the image directory of each theme.";
-$l['order'] = "Order";
-$l['display_order'] = "Display Order";
-$l['display_order_desc'] = "The order on the smilies list that this will appear. This number should not be the same as another smilie's.";
-$l['mass_edit_show_clickable'] = "Show on Clickable?";
-$l['show_clickable'] = "Show on clickable list?";
-$l['show_clickable_desc'] = "Do you want this smilie to show on the clickable smilie list on the post editor?";
-$l['include'] = "Add?";
-$l['path_to_images'] = "Path to Images";
-$l['path_to_images_desc'] = "This is the path to the folder that the images are in.";
-$l['smilie_delete'] = "Delete?";
-$l['save_smilie'] = "Save Smilie";
-$l['save_smilies'] = "Save Smilies";
-$l['show_smilies'] = "Show Smilies";
-$l['reset'] = "Reset";
+$l['error_missing_name'] = "Δεν εισαγάγατε όνομα για αυτό το χαμόγελο.";
+$l['error_missing_text_replacement'] = "Δεν εισαγάγατε κείμενο αντικατάστασης για αυτό το χαμόγελο.";
+$l['error_missing_path'] = "Δεν εισαγάγατε διαδρομή για αυτό το χαμόγελο.";
+$l['error_missing_path_multiple'] = "Δεν εισαγάγατε διαδρομή.";
+$l['error_missing_order'] = "Δεν εισαγάγατε σειρά εμφάνισης για αυτό το χαμόγελο.";
+$l['error_duplicate_order'] = "Δεν εισαγάγατε έγκυρη σειρά εμφάνισης για αυτό το χαμόγελο.";
+$l['error_missing_clickable'] = "Δεν καθορίσατε ναι ή όχι για την επιλογή \"Εμφάνιση σε Κλικάρισμα\".";
+$l['error_no_smilies'] = "Δεν υπάρχουν χαμόγελα στον καθορισμένο κατάλογο ή όλα τα χαμόγελα στον κατάλογο έχουν ήδη προστεθεί.";
+$l['error_no_images'] = "Δεν υπάρχουν εικόνες στον καθορισμένο κατάλογο.";
+$l['error_none_included'] = "Δεν επιλέξατε κανένα χαμόγελο για περιλαμβανόμενο.";
+$l['error_invalid_path'] = "Δεν εισαγάγατε έγκυρη διαδρομή.";
+$l['error_invalid_smilie'] = "Το καθορισμένο χαμόγελο δεν υπάρχει.";
 
-$l['error_missing_name'] = "You did not enter a name for this smilie.";
-$l['error_missing_text_replacement'] = "You did not enter a text replacement for this smilie.";
-$l['error_missing_path'] = "You did not enter a path for this smilie.";
-$l['error_missing_path_multiple'] = "You did not enter a path.";
-$l['error_missing_order'] = "You did not enter a display order for this smilie.";
-$l['error_duplicate_order'] = "You did not enter a valid display order for this smilie.";
-$l['error_missing_clickable'] = "You did not specify yes or no for the \"Show Clickable\" option.";
-$l['error_no_smilies'] = "There are no smilies in the specified directory, or all smilies in the directory have already been added.";
-$l['error_no_images'] = "There are no images in the specified directory.";
-$l['error_none_included'] = "You did not select any smilies to include.";
-$l['error_invalid_path'] = "You did not enter a valid path.";
-$l['error_invalid_smilie'] = "The specified smilie does not exist.";
+$l['success_smilie_added'] = "Το χαμόγελο προστέθηκε με επιτυχία.";
+$l['success_multiple_smilies_added'] = "Τα επιλεγμένα χαμόγελα προστέθηκαν με επιτυχία.";
+$l['success_smilie_updated'] = "Το χαμόγελο ενημερώθηκε με επιτυχία.";
+$l['success_multiple_smilies_updated'] = "Τα χαμόγελα ενημερώθηκαν με επιτυχία.";
+$l['success_smilie_deleted'] = "Το επιλεγμένο χαμόγελο διαγράφηκε με επιτυχία.";
+$l['success_mass_edit_updated'] = "Τα χαμόγελα ενημερώθηκαν με επιτυχία.";
 
-$l['success_smilie_added'] = "The smilie has been added successfully.";
-$l['success_multiple_smilies_added'] = "The selected smilies have been added successfully.";
-$l['success_smilie_updated'] = "The smilie has been updated successfully.";
-$l['success_multiple_smilies_updated'] = "The smilies have been updated successfully.";
-$l['success_smilie_deleted'] = "The selected smilie has been deleted successfully.";
-$l['success_mass_edit_updated'] = "The smilies have been updated successfully.";
-
-$l['confirm_smilie_deletion'] = "Are you sure you wish to delete this smilie?";
+$l['confirm_smilie_deletion'] = "Είστε βέβαιοι ότι θέλετε να διαγράψετε αυτό το χαμόγελο;";
 
